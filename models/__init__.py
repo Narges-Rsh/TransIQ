10c0d07407bc24f11ca58d0da30902510fc86c25
Narges-Rsh <134446921+Narges-Rsh@users.noreply.github.com>
2024-04-10 10:41:33 -0400

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,14 +1,8 @@
 from .ModelBase import ModelBase
-from .ResNet import ResNet
-from .CNN2 import CNN2
-from .DenseNet import DenseNet
-from .CLDNN import CLDNN
 from .TransIQ_Large_Variant import TransIQ_Large_Variant
 from .TransIQ_Small_Variant import TransIQ_Small_Variant
 from.TransIQ_Complex import TransIQ_Complex  
 from.TransDirect import TransDirect
-from.TransDirect_Overlapping import TransDirect_Overelapping
-from.VTCNN2 import VTCNN2  
+from.TransDirect_Overlapping import TransDirect_Overelapping 
 
-__all__ = ['ModelBase', 'ResNet', 'CLDNN','CNN2', 'VTCNN2','DenseNet',
-           'TransIQ_Large_Variant', 'TransIQ_Small_Variant','TransIQ_Complex','TransDirect', 'TransDirect_Overlapping']
+__all__ = ['ModelBase','TransIQ_Large_Variant', 'TransIQ_Small_Variant','TransIQ_Complex','TransDirect', 'TransDirect_Overlapping']
